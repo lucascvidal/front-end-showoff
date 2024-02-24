4d18aea1d218954319acc18d035b85c812d76a65
Lucas Vidal <lcv55@hotmail.com>
2024-02-24 16:51:29 -0300

    Adjust all animations

--- scripts.js
+++ scripts.js
@@ -2,32 +2,42 @@ document.addEventListener("DOMContentLoaded", function() {
   const glimpseButton = document.querySelector(".marketing-callout > h3");
   glimpseButton.addEventListener("click", hideMarketingCallout);
 
-  const activityFeed = document.querySelector('.activity-feed .crawl');
-  activityFeed.addEventListener('animationend', handleAnimationEnd);
+  const activityFeed = document.querySelector(".activity-feed .crawl");
+  activityFeed.addEventListener("animationend", handleAnimationEnd);
 });
 
 function handleAnimationEnd() {
-  document.querySelector('.activity-feed').style.display = 'none';
-
+  const activityFeed = document.querySelector(".activity-feed");
+  activityFeed.classList.add("opacity-zero");
+  
   const header = document.querySelector("header");
-  header.classList.toggle("display-none");
+  setTimeout(() => {
+    activityFeed.classList.add("display-none");
+    header.classList.remove("display-none");
+  }, 500);
+
+  setTimeout(() => {
+    header.classList.add("visible");
+  }, 1000);
 }
 
 function hideMarketingCallout() {
   const header = document.querySelector("header");
-  header.querySelector(".marketing-callout").classList.toggle("opacity-zero");
-  header.querySelector(".marketing-callout").classList.toggle("height-zero");
-  header.classList.toggle("height-zero");
-  header.querySelector(".marketing-callout").classList.toggle("display-none");
-  header.querySelector(".header-menu").classList.toggle("opacity-zero");
-  header.querySelector(".header-menu").classList.toggle("height-zero");
-  header.querySelector(".header-menu").classList.toggle("display-none");
+  header.classList.remove("visible");
+  header.classList.add("display-none");
 
   const dotifyGlimpseSection = document.querySelector(".dotify-glimpse");
-  dotifyGlimpseSection.classList.toggle("height-zero");
-  dotifyGlimpseSection.classList.toggle("opacity-zero");
-
   const audioPlayer = document.querySelector(".audio-player");
-  audioPlayer.classList.toggle("height-zero");
-  audioPlayer.classList.toggle("opacity-zero");
+  const footer = document.querySelector("footer");
+  setTimeout(() => {
+    dotifyGlimpseSection.classList.remove("display-none");
+    audioPlayer.classList.remove("display-none");
+    footer.classList.remove("display-none");
+  }, 500);
+  
+  setTimeout(() => {
+    dotifyGlimpseSection.classList.add("visible");
+    audioPlayer.classList.add("visible");
+    footer.classList.add("visible");
+  }, 1000);
 }
