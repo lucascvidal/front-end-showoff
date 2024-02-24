a8ea485ad029008b113d60924d3638c90cc6cd93
Lucas Vidal <lcv55@hotmail.com>
2024-02-23 21:18:32 -0300

    Add JS to handle animation end

--- scripts.js
+++ scripts.js
@@ -1,24 +1,33 @@
-window.onload = () => {
-  let glimpseButton = document.querySelector(".marketing-callout > h3");
+document.addEventListener("DOMContentLoaded", function() {
+  const glimpseButton = document.querySelector(".marketing-callout > h3");
+  glimpseButton.addEventListener("click", hideMarketingCallout);
 
-  if (glimpseButton) {
-    glimpseButton.addEventListener("click", () => {
-      let header = document.querySelector("header");
-      header.querySelector(".marketing-callout").classList.toggle("opacity-zero");
-      header.querySelector(".marketing-callout").classList.toggle("height-zero");
-      header.classList.toggle("height-zero");
-      header.querySelector(".marketing-callout").classList.toggle("display-none");
-      header.querySelector(".header-menu").classList.toggle("opacity-zero");
-      header.querySelector(".header-menu").classList.toggle("height-zero");
-      header.querySelector(".header-menu").classList.toggle("display-none");
+  const activityFeed = document.querySelector('.activity-feed .crawl');
+  activityFeed.addEventListener('animationend', handleAnimationEnd);
+});
 
-      let dotifyGlimpseSection = document.querySelector(".dotify-glimpse");
-      dotifyGlimpseSection.classList.toggle("height-zero");
-      dotifyGlimpseSection.classList.toggle("opacity-zero");
+function handleAnimationEnd() {
+  document.querySelector('.activity-feed').style.display = 'none';
 
-      let audioPlayer = document.querySelector(".audio-player");
-      audioPlayer.classList.toggle("height-zero");
-      audioPlayer.classList.toggle("opacity-zero");
-    });
-  }
-};
+  const header = document.querySelector("header");
+  header.classList.toggle("display-none");
+}
+
+function hideMarketingCallout() {
+  const header = document.querySelector("header");
+  header.querySelector(".marketing-callout").classList.toggle("opacity-zero");
+  header.querySelector(".marketing-callout").classList.toggle("height-zero");
+  header.classList.toggle("height-zero");
+  header.querySelector(".marketing-callout").classList.toggle("display-none");
+  header.querySelector(".header-menu").classList.toggle("opacity-zero");
+  header.querySelector(".header-menu").classList.toggle("height-zero");
+  header.querySelector(".header-menu").classList.toggle("display-none");
+
+  const dotifyGlimpseSection = document.querySelector(".dotify-glimpse");
+  dotifyGlimpseSection.classList.toggle("height-zero");
+  dotifyGlimpseSection.classList.toggle("opacity-zero");
+
+  const audioPlayer = document.querySelector(".audio-player");
+  audioPlayer.classList.toggle("height-zero");
+  audioPlayer.classList.toggle("opacity-zero");
+}
